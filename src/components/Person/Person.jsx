bc5a3c41a417718c08eb263020a4c119c3768593
Razsinxron <106243714+Razsinxron@users.noreply.github.com>
2024-10-31 20:38:32 +0200

    add correct content

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -13,8 +13,8 @@ export const Person = ({ person }) => {
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{person.name}</h2>
-      {person.age && <p className="Person__age">{person.age}</p>}
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
       <p className="Person__partner">{partnerContent}</p>
     </section>
   );
